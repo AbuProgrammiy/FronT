458a225d4b74295dea9d298a3b6c83fb91b7d6fa
Abu_Programmiy <abdukholiq0907@gmail.com>
2024-04-01 09:29:24 +0500

    ready

---
+++ assets/js-codes/index-jses.js
@@ -0,0 +1,9 @@
+function ShowVideo() {
+    var newDiv =document.getElementById("videoPlayer")
+    newDiv.style.display="block"
+}
+
+function closeVideo(){
+    var newDiv =document.getElementById("videoPlayer")
+    newDiv.style.display="none"
+}
